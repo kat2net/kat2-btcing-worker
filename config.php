e3a2ee2b5fd8c1ea35998cb0c13128015d7f5950
Kat2 <main@kat2.net>
2016-11-11 14:24:27 +1100

    0.0.0.3

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php');
 
-$v = '0.0.0.2';
+$v = '0.0.0.3';
 
 use BitWasp\Bitcoin\Bitcoin;
 use BitWasp\Bitcoin\Address;
@@ -12,19 +12,54 @@ function genAddr(){
     $privateKey = PrivateKeyFactory::create(true);
     $publicKey = $privateKey->getPublicKey();
 
-    $re = array(
-        'compressed' => $privateKey->isCompressed(),
-        'private' => array(
-            'wif' => $privateKey->toWif($network),
-            'hex' => $privateKey->getHex(),
-            'dec' => gmp_strval($privateKey->getSecret(), 10)
-        ),
-        'public' => array(
-            'wif' => $publicKey->getHex(),
-            'hex' => $publicKey->getPubKeyHash()->getHex(),
-            'dec' => $publicKey->getAddress()->getAddress()
-        )
-    );
+    $private_wif = $privateKey->toWif($network);
+    $private_hex = $privateKey->getHex();
+    $private_dec = gmp_strval($privateKey->getSecret(), 10);
+
+    $public_wif = $publicKey->getHex();
+    $public_hex = $publicKey->getPubKeyHash()->getHex();
+    $public_dec = $publicKey->getAddress()->getAddress();
+
+    $balance = getBal($public_dec);
+    if($balance['success']){
+        $balance = $balance['balance'];
+
+        $re = array(
+            'compressed' => $privateKey->isCompressed(),
+            'private' => array(
+                'wif' => $private_wif,
+                'hex' => $private_hex,
+                'dec' => $private_dec
+            ),
+            'public' => array(
+                'wif' => $public_wif,
+                'hex' => $public_hex,
+                'dec' => $public_dec
+            ),
+            'address' => $public_dec,
+            'balance' => $balance
+        );
+    }else{
+        return false;
+    }
+
+    return $re;
+}
+
+function getBalance($address){
+    $call = file_get_contents('https://blockchain.info/address/'.$address.'?format=json&limit=1');
+    $array = json_decode($call, true);
+
+    if($array['address'] == $address){
+        $re = array(
+            'success' => true,
+            'balance' => $array['final_balance']
+        );
+    }else{
+        $re = array(
+            'success' => false
+        );
+    }
 
     return $re;
 }
